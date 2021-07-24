5bf7a3f1bec07f8119e0db5c5bdabd75c5d9f0ae
sbr-vincent <vincent.g.sbr@gmail.com>
2021-07-23 17:37:29 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function over(element) {
+    // var x = document.getElementById("myVideo");
+    element.play();
+}
+
+function out(element) {
+    // var x = document.getElementById("myVideo");
+    element.pause();
+}
